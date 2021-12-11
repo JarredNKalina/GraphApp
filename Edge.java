12b09d9149865c6d838807f4f2ab676e966cee24
JarredNKalina <94949719+JarredNKalina@users.noreply.github.com>
2021-12-11 13:29:38 -0500

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Edge {
+    int src, dest, weight;
+    Edge(int src, int dest, int weight){
+        this.src= src;
+        this.weight = weight;
+        this.dest= dest;
+    }
+}
